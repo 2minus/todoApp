35e487aac57b30ff52c470db090c8eb0e99a589d
2minus <blecker0610@gmail.com>
2024-05-20 17:58:40 +0900

    feat(all) : TodoApp init & 일정 작성 API 코드 추가

---
+++ src/main/java/io/_2minus/todoapp/controller/TodoController.java
@@ -0,0 +1,16 @@
+package io._2minus.todoapp.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoController {
+
+    @PostMapping("/v1.0/todo")
+    public ResponseEntity postTodo() {
+        // TODO 일정 작성 기능
+        return ResponseEntity.ok().build();
+    }
+}
